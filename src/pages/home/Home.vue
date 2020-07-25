276888af850466e1dacfc73815f72713154de3d6
Harder99 <1485801278@qq.com>
2020-07-25 16:01:50 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
-    <router-view/>
+    Hello World
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
 
 <style>
 
 </style>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
