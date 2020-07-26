8b52a2d6197db57e0320b99dcf3045812c2f3340
Harder99 <1485801278@qq.com>
2020-07-26 22:26:35 +0800

    add city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,10 +5,13 @@
     </div>
     <div class="header-input">
         <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题</div>
-    <div class="header-right">{{this.city}}
-        <span class="iconfont down-icon">&#xe64a;</span>
+        输入城市/景点/游玩主题
     </div>
+    <router-link to="/city">
+      <div class="header-right">{{this.city}}
+        <span class="iconfont down-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -25,7 +28,7 @@ export default {
     @import '~styles/varibles.styl'
     .header
         display flex
-        line-height .86rem
+        line-height $headHeight
         background $bgColor
         color #fff
         .header-left
@@ -48,6 +51,7 @@ export default {
             width 1.24rem
             float right
             text-align center
+            color #fff
             .down-icon
                 font-size .24rem
                 margin-left -.04rem
